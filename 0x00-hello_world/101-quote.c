f415d069e39525b8e825195da0c2c18aacabf3dd
trevor2012 <thvambe@gmail.com>
2023-07-13 14:09:14 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,7 @@
 #include <unistd.h>
 /**
- * main - 101-quote.c 
- * description - Write a C program that prints exactly and 
- * that piece of art is useful\" - Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error.
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
